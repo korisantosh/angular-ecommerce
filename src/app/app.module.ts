8224fc3ac8c41d54ad5f6a4753770d970d2667ec
BojanRatkovic <ratkovicbojan94@gmail.com>
2018-06-01 14:22:35 +0200

    Initial commit, installed firebase.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
+import { firebase } from './../environments/firebase';
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+
+
 import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule,
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/environments/firebase.ts
@@ -0,0 +1,8 @@
+export const firebase = {
+    apiKey: "AIzaSyCayK0rdjWqtKwTN8TQduY204hED6KueFE",
+    authDomain: "oshop-be60c.firebaseapp.com",
+    databaseURL: "https://oshop-be60c.firebaseio.com",
+    projectId: "oshop-be60c",
+    storageBucket: "",
+    messagingSenderId: "481035585968"
+  };
